4ed4d1149a76fdbf767b6cf8bc98867aa361ef8f
offset-hacker444 <92826103+offset-hacker444@users.noreply.github.com>
2021-10-19 23:49:42 +0200

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,14 +29,14 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('👾 searching...')
+        M.reply('🌺 searching...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `👾Handcrafted for you by Kaoi👾`,
+                        body: `⚕️ Belongs to offset hacker⚕️`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
